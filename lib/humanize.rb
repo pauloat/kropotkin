295404cb53fb8ef08575d4c00ed2b6d4ea1772ac
Nicolás Reynolds <fauno@endefensadelsl.org>
2013-09-05 14:16:25 -0300

    Humanizar los reply

---
+++ lib/humanize.rb
@@ -0,0 +1,17 @@
+require 'cinch/timer'
+# Agregar un temporizador para que las respuestas no sean inmediatas y
+# parezca más humano
+module Cinch
+  class Message
+    def human_reply(text, prefix = false)
+      text = text.to_s
+      if @channel && prefix
+        text = text.split("\n").map {|l| "#{user.nick}: #{l}"}.join("\n")
+      end
+
+      Timer.new(rand(10), shots: 1) {
+        @target.send(text)
+      }
+    end
+  end
+end
