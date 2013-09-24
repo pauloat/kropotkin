03809e7eca73b4dec332a73335b1f5746fa292c3
Nicolás Reynolds <fauno@endefensadelsl.org>
2013-09-24 13:04:17 -0300

    Convertir entidades HTML a texto

--- lib/url_title.rb
+++ lib/url_title.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'net/https'
 require 'opengraph'
+require 'cgi'
 
 class UrlTitle
   include Cinch::Plugin
@@ -28,11 +29,11 @@ class UrlTitle
       debug "Fetching #{url}"
 
       if resource
-        m.reply "#{m.user.nick}: #{resource.title}" if not resource.title.empty?
+        m.reply "#{m.user.nick}: #{CGI.unescapeHTML(resource.title)}" if not resource.title.empty?
         m.reply resource.description if not resource.description.empty?
       else
         title = Net::HTTP.get(uri).gsub(/\n/, ' ').squeeze(' ').scan(/<title>(.*?)<\/title>/)[0][0]
-        m.reply "#{m.user.nick}: #{title}" if not title.empty?
+        m.reply "#{m.user.nick}: #{CGI.unescapeHTML(title)}" if not title.empty?
       end
     end
   end
